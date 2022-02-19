a26b2a0284ff2cd58fc8281dadaaef908fd9c729
ngong123 <nathan.gong123@gmail.com>
2022-02-18 23:41:39 -0800

    Commit code

--- helloWorld.py
+++ helloWorld.py
@@ -5,3 +5,11 @@ print("Nathan's second commit")
 #Trying to ask
 print("hi there")
 print("Julian's commit")
+
+
+# nathan is commiting another file after pulling julian's commit
+for i in range(3):
+    print(i)
+
+
+print("we are making a website")
